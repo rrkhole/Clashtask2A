1e46a621959cdd9820c6b6589c8030a854783e43
rrkhole <62331204+rrkhole@users.noreply.github.com>
2020-08-15 14:38:55 +0530

    Update urls.py

--- task2A/task_app/urls.py
+++ task2A/task_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('page1/',views.page1,name='page1'),
+    path('',views.page1,name='page1'),
     path('page2/',views.page2,name='page2'),
 ]
